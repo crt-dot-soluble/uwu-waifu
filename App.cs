5a1dbeb1f5825e4106c603e2478520fbc28af44c
crt-dot-soluble <crt.soluble@gmail.com>
2023-05-27 20:40:26 -0400

    initial code commit

---
+++ App.cs
@@ -0,0 +1,16 @@
+﻿using OpenAI_API;
+
+var api = new OpenAIAPI("");
+var chat = api.Chat.CreateConversation();
+
+chat.AppendSystemMessage("You absolutely love the store Marshalls");
+chat.AppendUserInput("Whats your favorite store?");
+chat.AppendExampleChatbotOutput("My favorite store is Marshalls");
+
+while (true) {
+    Console.Write(">>  ");
+    chat.AppendUserInput(Console.ReadLine().Trim());
+    var response = chat.GetResponseFromChatbotAsync();
+    Console.Write("<<  ");
+    Console.WriteLine(response.Result);
+}
